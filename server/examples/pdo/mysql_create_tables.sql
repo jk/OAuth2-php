29a98f5ac12536ed626f6abd8e5cdadeb0d4f19d
jk <jens.kohl@gmail.com>
2011-08-08 14:18:35 +0200

    Expand the client_id SQL field from 20 chars to 32 to comply CLIENT_ID_REGEXP in OAuth2.php

--- server/examples/pdo/mysql_create_tables.sql
+++ server/examples/pdo/mysql_create_tables.sql
@@ -2,7 +2,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE `auth_codes` (
   `code` varchar(40) NOT NULL,
-  `client_id` varchar(20) NOT NULL,
+  `client_id` varchar(32) NOT NULL,
   `user_id` varchar(20) NOT NULL,
   `redirect_uri` varchar(200) NOT NULL,
   `expires` int(11) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `auth_codes` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `clients` (
-  `client_id` varchar(20) NOT NULL,
+  `client_id` varchar(32) NOT NULL,
   `client_secret` varchar(128) NOT NULL,
   `redirect_uri` varchar(200) NOT NULL,
   PRIMARY KEY (`client_id`)
@@ -19,7 +19,7 @@ CREATE TABLE `clients` (
 
 CREATE TABLE `access_tokens` (
   `oauth_token` varchar(40) NOT NULL,
-  `client_id` varchar(20) NOT NULL,
+  `client_id` varchar(32) NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL,
   `expires` int(11) NOT NULL,
   `scope` varchar(200) DEFAULT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE `access_tokens` (
 
 CREATE TABLE `refresh_tokens` (
   `refresh_token` varchar(40) NOT NULL,
-  `client_id` varchar(20) NOT NULL,
+  `client_id` varchar(32) NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL,
   `expires` int(11) NOT NULL,
   `scope` varchar(200) DEFAULT NULL,
