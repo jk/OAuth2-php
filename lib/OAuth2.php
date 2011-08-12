0fdbe43b9b1e784bc2d4068e926f75d6d94169ef
jk <jens.kohl@gmail.com>
2011-08-12 14:55:38 +0200

    Fixes two PHP notices when accessing non-setted PHP_AUTH vars
    1.) Notice:  Undefined index: PHP_AUTH_USER in /Users/jkohl/Sites/vbox/sites/oauth2-draft20/lib/OAuth2.php on line 1034
    2.) Notice:  Undefined index: PHP_AUTH_PW in /Users/jkohl/Sites/vbox/sites/oauth2-draft20/lib/OAuth2.php on line 1035

--- lib/OAuth2.php
+++ lib/OAuth2.php
@@ -1030,10 +1030,10 @@ class OAuth2 {
    * @ingroup oauth2_section_3
    */
   protected function getAuthorizationHeader() {
-    return array(
-      'PHP_AUTH_USER' => $_SERVER['PHP_AUTH_USER'],
-      'PHP_AUTH_PW'   => $_SERVER['PHP_AUTH_PW']    
-    );
+	$returnArray = array();
+	$returnArray['PHP_AUTH_USER'] = (isset($_SERVER['PHP_AUTH_USER'])) ? $_SERVER['PHP_AUTH_USER'] : '';
+	$returnArray['PHP_AUTH_PW'] = (isset($_SERVER['PHP_AUTH_PW'])) ? $_SERVER['PHP_AUTH_PW'] : '';
+    return $returnArray;
   }
 
   /**
